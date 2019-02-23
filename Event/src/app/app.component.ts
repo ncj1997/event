e9b3a47a666d17dbf43e2eb9eb4d9459f829407b
ncj1997 <nishshankacj@gmail.com>
2019-02-23 19:53:07 +0530

    adding part 2

---
+++ Event/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  topics= ['Sharmadana Campain','Exhibihition','School Developmt Event','Others'];
+  moderators=['person A','person B'];
+}
